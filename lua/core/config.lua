6e144aced527abbc7f2335d6f62be5843b567e97
Mechanical Beaver <pyroasperin@gmail.com>
2025-05-03 00:09:03 +0500

    updeted

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -8,3 +8,8 @@ vim.opt.shiftwidth = 4
 --vim.o.termguicolors = true
 --vim.o.cursorline = true
 --vim.o.number = true
+
+
+if vim.fn.argc() == 0 then
+    vim.cmd("Bdelete")
+end
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,9 @@
 require("neo-tree").setup({
     lazy = false,
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+        },
+    },
 })
-
