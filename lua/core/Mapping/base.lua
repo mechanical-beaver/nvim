47b15ef59c93acfb7bfb070c4b123b1ebe60c954
Mechanical Beaver <pyroasperin@gmail.com>
2025-06-29 23:32:52 +0500

    updated

--- lua/core/Mapping/base.lua
+++ lua/core/Mapping/base.lua
@@ -5,19 +5,19 @@ local opts = { noremap = true, silent = true }
 
 keyset("n", "<leader>w", ":w<CR>", opts)
 
-keyset({ "n", "v", "i" }, "<C-Down>", "<Esc> :wincmd j<CR>", opts)
-keyset({ "n", "v", "i" }, "<C-Up>", "<Esc> :wincmd k<CR>", opts)
-keyset({ "n", "v", "i" }, "<C-Left>", "<Esc> :wincmd h<CR>", opts)
-keyset({ "n", "v", "i" }, "<C-Right>", "<Esc> :wincmd l<CR>", opts)
+keyset({ "n", "v", "i" }, "<C-Down>", "<cmd>:wincmd j<CR>", opts)
+keyset({ "n", "v", "i" }, "<C-Up>", "<cmd>:wincmd k<CR>", opts)
+keyset({ "n", "v", "i" }, "<C-Left>", "<cmd>:wincmd h<CR>", opts)
+keyset({ "n", "v", "i" }, "<C-Right>", "<cmd>:wincmd l<CR>", opts)
 
-keyset("n", "<C-y><C-y>", '"+yy')
-keyset("n", "<C-p><C-p>", '"+p')
-keyset("v", "<C-y><C-y>", '"+y')
-keyset("v", "<C-p><C-p>", '"+p')
-keyset("i", "<C-p><C-p>", "<C-r>+")
+keyset("n", "<C-c>", '"+yy')
+keyset("n", "<C-v>", '"+p')
+keyset("v", "<C-c>", '"+y')
+keyset("v", "<C-v>", '"+p')
+keyset("i", "<C-v>", "<C-r>+")
 
 keyset({ "n", "v", "i" }, "<S-Down>", "<Down><Down><Down><Down><Down>")
 keyset({ "n", "v", "i" }, "<S-Up>", "<Up><Up><Up><Up><Up>")
 
-keyset("i", "<C-r>", "<C-o><C-r>")
-keyset("i", "<C-u>", "<Esc>ua")
+keyset("i", "<C-r>", "<cmd>redo<CR>")
+keyset("i", "<C-z>", "<cmd>undo<CR>")
--- lua/core/Mapping/plugins.lua
+++ lua/core/Mapping/plugins.lua
@@ -18,10 +18,20 @@ M.commands_mapping = function()
     keyset("n", "<leader>x", ":Bdelete<CR>", tbl("force", opts, { desc = "Close buffer" }))
 
     ------------------hop
-    keyset("n", "<leader>q1", ":HopChar1<CR>", tbl("force", opts, { desc = "Select one char to jump" }))
-    keyset("n", "<leader>q2", ":HopChar2<CR>", tbl("force", opts, { desc = "Select two chars to jump" }))
-    keyset("n", "<leader>qw", ":HopWord<CR>", tbl("force", opts, { desc = "Choose a word to jump" }))
-    keyset("n", "<leader>ql", ":HopLine<CR>", tbl("force", opts, { desc = "Select a line to jump" }))
+    keyset("n", "<leader>f1", ":HopChar1<CR>", tbl("force", opts, { desc = "Select one char to jump" }))
+    keyset("n", "<leader>f2", ":HopChar2<CR>", tbl("force", opts, { desc = "Select two chars to jump" }))
+    keyset("n", "<leader>fw", ":HopWord<CR>", tbl("force", opts, { desc = "Choose a word to jump" }))
+    keyset("n", "<leader>fl", ":HopLine<CR>", tbl("force", opts, { desc = "Select a line to jump" }))
+
+    ------------------comments
+    keyset("i", "<M-/>", function()
+        local api = require("Comment.api")
+        local row = vim.api.nvim_win_get_cursor(0)[1]
+        api.toggle.linewise.current(row)
+    end, { desc = "Toggle comment in Insert mode (safely)" })
+
+    ------------------snacks
+    keyset("n", "<leader>dd", "<cmd>lua Snacks.dashboard()<CR>", tbl("force", opts, { desc = "Open dashboard" }))
 end
 ------------------cmp
 M.cmp_mapping = function()
@@ -64,10 +74,10 @@ end
 ------------------comments
 M.comments_mapping = {
     toggler = {
-        line = "cc",
+        line = "//",
     },
     opleader = {
-        line = "c",
+        line = "/",
     },
 }
 
